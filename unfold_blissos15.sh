ef248c0bb95a339e43eaf6ff3264c82f93d1ad89
Jon West <electrikjesus@gmail.com>
2025-03-31 12:55:16 -0400

    bass: a12.1: manifest and script updates

--- unfold_blissos15.sh
+++ unfold_blissos15.sh
@@ -34,7 +34,7 @@ git submodule update --init --recursive
 
 echo -e "${ltblue}Init repo tree using AOSP manifest ${reset}"
 pushd aosptree
-repo init -u https://github.com/BlissOS/platform_manifest.git -b arcadia-x86 --git-lfs
+repo init -u https://github.com/BlissROMs-x86/manifest.git -b arcadia-x86 --git-lfs
 cd .repo/manifests
 rm default.xml
 cp ${LOCAL_PATH}/manifests/blissos15-static.xml bliss.xml
