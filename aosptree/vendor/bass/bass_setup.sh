0062f3100cb6a4dc6709893925558464149211cc
Jon West <electrikjesus@gmail.com>
2025-07-17 09:38:48 -0400

    bass: let ksu switch also handle legacy builds

--- aosptree/vendor/bass/bass_setup.sh
+++ aosptree/vendor/bass/bass_setup.sh
@@ -627,11 +627,13 @@ function copy_configs()
 
     if [ "$BLISS_REMOVE_KSU" = "true" ]; then
         echo "Removing KSU config from kernel"
+        sed -i 's/CONFIG_KSU=y/\# CONFIG_KSU is not set/' kernel/x86/arch/x86/configs/android-x86_64_defconfig
         sed -i 's/CONFIG_KSU=y/\# CONFIG_KSU is not set/' kernel/x86/common/arch/x86/configs/android-x86_64_defconfig
         sed -i 's/CONFIG_KSU=y/\# CONFIG_KSU is not set/' kernel/x86/surface/arch/x86/configs/android-x86_64_defconfig
         sed -i 's/CONFIG_KSU=y/\# CONFIG_KSU is not set/' kernel/x86/zenith/arch/x86/configs/android-x86_64_defconfig
     else
         echo "Adding KSU config to kernel"
+        sed -i 's/\# CONFIG_KSU is not set/CONFIG_KSU=y/' kernel/x86/arch/x86/configs/android-x86_64_defconfig
         sed -i 's/\# CONFIG_KSU is not set/CONFIG_KSU=y/' kernel/x86/common/arch/x86/configs/android-x86_64_defconfig
         sed -i 's/\# CONFIG_KSU is not set/CONFIG_KSU=y/' kernel/x86/surface/arch/x86/configs/android-x86_64_defconfig
         sed -i 's/\# CONFIG_KSU is not set/CONFIG_KSU=y/' kernel/x86/zenith/arch/x86/configs/android-x86_64_defconfig
