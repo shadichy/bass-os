ff721387a4ab04cc7418fedfe140319877f4843e
Jon West <electrikjesus@gmail.com>
2025-07-22 20:01:27 -0400

    bass: Updates for bootsight v2.02

--- aosptree/vendor/bass/bass_setup.sh
+++ aosptree/vendor/bass/bass_setup.sh
@@ -307,7 +307,7 @@ function validity_checks()
     fi
     # check that the bootsight files all match the right hashsums
     if [ -f vendor/bass/prebuilts/bootsight/prebuilts/unsigned-priv-api-apps/BootSight.apk ]; then
-        if [ ! $(sha256sum vendor/bass/prebuilts/bootsight/prebuilts/unsigned-priv-api-apps/BootSight.apk | grep -c "a530af3cc0eecbb5e92c227e270be3cf840a4bea3ba8ff8ed7c761dc332b0fa4") == 1 ]; then
+        if [ ! $(sha256sum vendor/bass/prebuilts/bootsight/prebuilts/unsigned-priv-api-apps/BootSight.apk | grep -c "d6ce6b3138b7d658d6eab7d1cf4dcaa84c58e311a21f3f74478287d41b852f64") == 1 ]; then
             tampered_bootsight=1
             echo " - BootSight.apk" > ../bass_validity_check.log
         fi
